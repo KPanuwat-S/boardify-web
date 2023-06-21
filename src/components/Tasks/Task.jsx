8dd6a4a4436017907c44b01fd895bef099052ef9
KPanuwat-S <panuwatbood@gmail.com>
2023-06-21 14:08:05 +0700

    Member & Task

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 import DropdownTask from "./DropDownTask";
+import ReactQuill from "react-quill";
 
 function Task() {
   const [open, setOpen] = useState(false);
-
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("Title");
+  const [isEdit, setIsEdit] = useState(false);
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      // [{ font: [] }],
+      // [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+    ],
+  };
   return (
     <div className="bg-gray-700 ">
       <div className="flex bg-gray-100 w-[760px] mx-auto p-5 rounded-[4px]">
@@ -12,7 +29,26 @@ function Task() {
           <div>
             <div className="flex gap-5 items-center">
               <i class="fa-solid fa-bars-progress text-gray-500"></i>
-              <h1>Task Name</h1>
+              <h1>
+                {isEdit ? (
+                  <input
+                    className="px-2"
+                    type="text"
+                    value={title}
+                    onChange={(e) => {
+                      setTitle(e.target.value);
+                    }}
+                  />
+                ) : (
+                  <h1
+                    onClick={() => {
+                      setIsEdit(true);
+                    }}
+                  >
+                    {title}
+                  </h1>
+                )}
+              </h1>
             </div>
             <p className="font-light text-[14px]">In Card: card name</p>
           </div>
@@ -24,7 +60,7 @@ function Task() {
             <div className="flex gap-5 ml-10">
               {!open && (
                 <div
-                  className="p-2 w-[450px] text-gray-400 bg-gray-200 rounded-[4px] hover:bg-gray-300"
+                  className="p-2 w-[500px] text-gray-400 bg-gray-200 rounded-[4px] hover:bg-gray-300"
                   role="button"
                   onClick={() => {
                     setOpen(true);
@@ -36,7 +72,16 @@ function Task() {
 
               {open && (
                 <div className="flex flex-col gap-2 ">
-                  <div className="bg-gray-200 w-[450px] h-[200px] rounded-[4px]"></div>
+                  <div className="w-[500px]">
+                    <ReactQuill
+                      theme="snow"
+                      value={description}
+                      onChange={setDescription}
+                      className="editor-input"
+                      modules={modules}
+                      bounds="#parent"
+                    />
+                  </div>
                   <div
                     onClick={() => {
                       setOpen(false);
