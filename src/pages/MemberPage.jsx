e9fa15538a343a7841d2df08d94879a24d236698
tharathep-k <sun.tharathep@gmail.com>
2023-06-22 12:28:56 +0700

    start:Member

--- src/pages/MemberPage.jsx
+++ src/pages/MemberPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Modal from "../components/Modal";
+import MemberCard from "../features/member/components/MemberCard";
 
 function MemberPage() {
   const { id } = useParams();
@@ -42,26 +43,8 @@ function MemberPage() {
 
       {/* Member Component */}
       <div className="flex flex-col  mt-10">
-        <div className="flex flex-1 gap-5 justify-between">
-          <div className="flex gap-5">
-            <div className="flex items-center justify-center w-[40px] h-[40px] bg-blue-400 text-white rounded-full">
-              SD
-            </div>
-            <div>
-              <h2 className="font-bold">Sadana Doe</h2>
-              <h3>sadananDoe@gmail.com</h3>
-            </div>
-          </div>
-          <div className="flex items-center gap-5 ">
-            <div>
-              <h2 className="text-blue-600">On 2 Boards</h2>
-            </div>
-            <div>Admin</div>
-            <button className="flex items-center px-2 py-1 bg-gray-100 rounded-[4px] hover:bg-gray-200 duration-200">
-              <h2 className="text-gray-500">Remove</h2>
-            </button>
-          </div>
-        </div>
+        {/* Member Card */}
+        <MemberCard />
       </div>
     </div>
   );
