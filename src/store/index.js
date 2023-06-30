f9bfc5c0fddd7ec16d2c00f6e8dceb7f9c12758c
K.Panuwat <128443056+KPanuwat-S@users.noreply.github.com>
2023-06-30 15:31:43 +0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/Slice/authSlice";
 import cardReducer from "../features/board/card/Slice/cardSlice";
 import workspaceReducer from "../features/workspace/Slice/workspaceSlice";
 import boardReducer from "../features/board/board/Slice/boardSlice";
